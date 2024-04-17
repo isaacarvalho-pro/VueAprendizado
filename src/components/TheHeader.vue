dd10c4e0ec33814024a0e90836320b7f096961be
isaacarvalho-pro <isaacarvalho_pro@outlook.com>
2024-04-17 02:03:47 -0300

    Primeiro commit vue, aprendendo sobre component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="TheHeader">
+        header
+    </header>
+</template>
+
+
+<script>
+    export default {
+
+    }
+</script>
+
+
+<style>
+    .TheHeader{
+        background-color: black;
+        color: white;
+    }
+</style>
